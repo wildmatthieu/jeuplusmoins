024dbd00cb8d5becf31817003284af8e57a10f73
wildmatthieu <matthieu.lopez@wildcodeschool.com>
2024-03-06 14:46:35 +0100

    core implemented

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ const playerName = prompt("quel est votre nom ?");
 
 const numberToGuess = Math.ceil(Math.random() * 100);
 
+
+let playerNumber = null;
+
+// tant que le nombre du joueur n'est pas égal au nombre à deviner
+while(playerNumber !== numberToGuess)
+{
+    playerNumber = parseInt(prompt("Devinez le nombre"));
+
+    // si le joueur devine le bon nombre
+    if(playerNumber === numberToGuess)
+    {
+        // alors il a gagné
+        alert('Bravo ' + playerName + ' !')
+    } 
+    // sinon si le nombre du joueur est trop petit
+    else if(playerNumber < numberToGuess)
+    {
+        // alors on lui dit que le nombre a deviner est plus grand
+        alert('Le nombre a deviner est plus grand !');
+    }
+    else
+    {
+        alert('Le nombre a deviner est plus petit !');
+    }
+}
+
+
